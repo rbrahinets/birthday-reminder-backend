0846d14542e634c1011492d2e74ef5c5e5ede031
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-19 13:56:41 +0300

    Delete demo code

--- src/server.js
+++ src/server.js
@@ -7,12 +7,6 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.json({
-        title: 'Hello friend',
-    });
-});
-
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });

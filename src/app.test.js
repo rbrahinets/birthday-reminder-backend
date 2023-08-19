936808d8dee9507ae1b29df1b1e44306aee2c81c
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-19 18:28:58 +0300

    Add tests for request GET /api/v1/users/:id

--- src/app.test.js
+++ src/app.test.js
@@ -2,9 +2,11 @@ const request = require('supertest');
 const makeApp = require('./app');
 
 const findUsers = jest.fn();
+const findUser = jest.fn();
 
 const app = makeApp({
     findUsers,
+    findUser,
 });
 
 describe('GET /api/v1/users', () => {
@@ -34,5 +36,52 @@ describe('GET /api/v1/users', () => {
         await getResponse();
 
         expect(findUsers.mock.calls.length).toBe(1);
+    });
+});
+
+describe('GET /api/v1/users/:id', () => {
+    const getResponse = async (id) => {
+        return await request(app).get(`/api/v1/users/${id}`);
+    };
+
+    beforeEach(async () => {
+        findUser.mockReset();
+    });
+
+    test('Should respond with a 200 status code if user found', async () => {
+        findUser.mockResolvedValue({
+            _id: 1,
+            firstName: 'firstName',
+            lastName: 'lastName',
+            email: 'email',
+            password: 'password',
+        });
+
+        const response = await getResponse(1);
+
+        expect(response.statusCode).toBe(200);
+    });
+
+    test('Should respond with a 404 status code if user not found', async () => {
+        findUser.mockResolvedValue(null);
+
+        const response = await getResponse(0);
+
+        expect(response.statusCode).toBe(404);
+        expect(response.body).toEqual({ message: 'User Not Found' });
+    });
+
+    test('Should specify json in the content type header', async () => {
+        const response = await getResponse(1);
+
+        expect(response.headers['content-type']).toEqual(
+            expect.stringContaining('json')
+        );
+    });
+
+    test('Should get user from database', async () => {
+        await getResponse(1);
+
+        expect(findUser.mock.calls.length).toBe(1);
     });
 });

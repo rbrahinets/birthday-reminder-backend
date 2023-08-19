afaa1281cae50ab0dc5f764054fb51eef664cb77
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-19 17:16:12 +0300

    Add tests for request GET /api/v1/users

---
+++ src/app.test.js
@@ -0,0 +1,38 @@
+const request = require('supertest');
+const makeApp = require('./app');
+
+const findUsers = jest.fn();
+
+const app = makeApp({
+    findUsers,
+});
+
+describe('GET /api/v1/users', () => {
+    const getResponse = async () => {
+        return await request(app).get('/api/v1/users');
+    };
+
+    beforeEach(async () => {
+        findUsers.mockReset();
+    });
+
+    test('Should respond with a 200 status code', async () => {
+        const response = await getResponse();
+
+        expect(response.statusCode).toBe(200);
+    });
+
+    test('Should specify json in the content type header', async () => {
+        const response = await getResponse();
+
+        expect(response.headers['content-type']).toEqual(
+            expect.stringContaining('json')
+        );
+    });
+
+    test('Should get users from database', async () => {
+        await getResponse();
+
+        expect(findUsers.mock.calls.length).toBe(1);
+    });
+});

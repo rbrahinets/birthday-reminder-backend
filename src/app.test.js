ef534f482f1d84a1532580f55c2caa8392b2f5f9
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-19 21:11:09 +0300

    Add tests for request POST /api/v1/users

--- src/app.test.js
+++ src/app.test.js
@@ -3,10 +3,12 @@ const makeApp = require('./app');
 
 const findUsers = jest.fn();
 const findUser = jest.fn();
+const saveUser = jest.fn();
 
 const app = makeApp({
     findUsers,
     findUser,
+    saveUser,
 });
 
 describe('GET /api/v1/users', () => {
@@ -83,5 +85,144 @@ describe('GET /api/v1/users/:id', () => {
         await getResponse(1);
 
         expect(findUser.mock.calls.length).toBe(1);
+    });
+});
+
+describe('POST /api/v1/users', () => {
+    const newUser = {
+        firstName: 'firstName',
+        lastName: 'lastName',
+        email: 'test@email.com',
+        password: 'password',
+    };
+
+    const getResponse = async () => {
+        return await request(app).post('/api/v1/users').send(newUser);
+    };
+
+    beforeEach(async () => {
+        saveUser.mockReset();
+    });
+
+    describe('When given a user data', () => {
+        test('Should respond with a 201 status code', async () => {
+            findUsers.mockResolvedValue([
+                { ...newUser, id: '13', email: 'existed@email.com' },
+            ]);
+
+            const response = await getResponse();
+
+            expect(response.statusCode).toBe(201);
+        });
+
+        test('Should specify json in the content type header', async () => {
+            findUsers.mockResolvedValue([
+                { ...newUser, id: '13', email: 'existed@email.com' },
+            ]);
+
+            const response = await getResponse();
+
+            expect(response.headers['content-type']).toEqual(
+                expect.stringContaining('json')
+            );
+        });
+
+        test('Should save the user to the database', async () => {
+            findUsers.mockResolvedValue([
+                { ...newUser, id: '13', email: 'existed@email.com' },
+            ]);
+
+            await getResponse();
+
+            expect(saveUser.mock.calls.length).toBe(1);
+        });
+
+        test('Should respond with a json object contain the id', async () => {
+            findUsers.mockResolvedValue([
+                { ...newUser, id: '13', email: 'existed@email.com' },
+            ]);
+            saveUser.mockResolvedValue({ id: '1' });
+
+            const response = await getResponse();
+
+            expect(response.body.id).toBe('1');
+        });
+
+        test('Should respond with 400 status code if email is already in use', async () => {
+            findUsers.mockResolvedValue([{ id: '13', ...newUser }]);
+
+            const response = await getResponse();
+
+            expect(response.statusCode).toBe(400);
+            expect(response.body).toEqual({
+                message: 'Email Is Already In Use',
+            });
+        });
+    });
+
+    describe('When the user data is missing', () => {
+        test('Shold respond with a 400 status code', async () => {
+            const bodyData = [
+                {
+                    lastName: 'lastName',
+                    email: 'test@email.com',
+                    password: 'password',
+                },
+                {
+                    firstName: 'firstName',
+                    email: 'test@email.com',
+                    password: 'password',
+                },
+                {
+                    firstName: 'firstName',
+                    lastName: 'lastName',
+                    password: 'password',
+                },
+                {
+                    firstName: 'firstName',
+                    lastName: 'lastName',
+                    email: 'test@email.com',
+                },
+                {
+                    firstName: ' ',
+                    lastName: 'lastName',
+                    email: 'test@email.com',
+                    password: 'password',
+                },
+                {
+                    firstName: 'firstName',
+                    lastName: ' ',
+                    email: 'test@email.com',
+                    password: 'password',
+                },
+                {
+                    firstName: 'firstName',
+                    lastName: 'lastName',
+                    email: ' ',
+                    password: 'password',
+                },
+                {
+                    firstName: 'firstName',
+                    lastName: 'lastName',
+                    email: 'test@email.com',
+                    password: ' ',
+                },
+            ];
+
+            for (const body of bodyData) {
+                findUsers.mockResolvedValue([
+                    { ...newUser, id: '13', email: 'existed@email.com' },
+                ]);
+
+                const response = await request(app)
+                    .post('/api/v1/users')
+                    .send(body);
+
+                expect(response.statusCode).toBe(400);
+                expect(response.body).toEqual({
+                    message: 'User Data Is Missing',
+                });
+            }
+        });
     });
 });

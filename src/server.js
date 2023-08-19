b1f499107efec2dcbea8e7bb68c61840b1f4f434
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-19 17:03:27 +0300

    Add db to server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const makeApp = require('./app');
+const dotenv = require('dotenv').config();
+const connectDb = require('./config/dbConnection');
+const db = require('./db/db');
+
+connectDb();
 
 const PORT = process.env.PORT || 8080;
-const app = makeApp();
+const app = makeApp(db);
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);

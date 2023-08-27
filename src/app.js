5c66d374101c74cf744e60367202a00946261c8d
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-27 15:29:42 +0300

    Move userRouter to routers directory

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const userRouter = require('./userRouter');
+const userRouter = require('./routers/userRouter');
 
 const app = express();
 

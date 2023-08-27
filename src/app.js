3d16485bf4016a4a923ad0789e5dc10c1415b2f7
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-27 15:59:24 +0300

    Add handler for user routes to modeule app

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use('/api/v1/users', require('./routes/userRoutes'));
 
 module.exports = (db) => {
     app.set(userRouter(app, db));

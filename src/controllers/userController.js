bc5b034689741b2dc254ff49b5dab48e4856d21e
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-27 15:55:55 +0300

    Add controller sign in

---
+++ src/controllers/userController.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const db = require('../db/db');
+
+const signInUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ message: 'User Data Is Missing' });
+    }
+
+    const user = await db.findUserByEmail(email);
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign(
+            {
+                id: user.id,
+                username: `${user.firstName} ${user.lastName}`,
+                email: email,
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '1m' }
+        );
+
+        res.status(200).json({ accessToken });
+    } else {
+        res.status(401).json({ message: 'Email Or Password Is Not Valid' });
+    }
+};
+
+module.exports = {
+    getCurrentUser,
+    signInUser,
+};

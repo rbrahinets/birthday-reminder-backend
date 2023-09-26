ddf08c232eac8260bbb1efbf756bd9ebfe34f5c1
rbrahinets <rostyslav.brahinets@gmail.com>
2023-09-26 17:35:20 +0300

    Add friend router to app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const userRouter = require('./routers/userRouter');
+const friendRouter = require('./routers/friendRouter');
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use('/api/v1/users', require('./routes/userRoutes'));
 
 module.exports = (db) => {
     app.set(userRouter(app, db));
+    app.set(friendRouter(app, db));
 
     return app;
 };

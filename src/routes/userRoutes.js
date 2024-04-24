31048fe589a99b8dddde1feb7d3dfad43f45256e
rbrahinets <rostyslav.barhinets@gmail.com>
2024-04-24 19:19:27 +0300

    Update routes for user

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,25 @@
 const express = require('express');
-const { getCurrentUser, signInUser } = require('../controllers/userController');
-const validateToken = require('../middleware/validateTokenHandler');
+const validateToken = require('../middlewares/validateTokenHandler');
+const {
+    getUsers,
+    getUser,
+    saveUser,
+    updateUser,
+    deleteUser,
+    getUserByEmail,
+    getCurrentUser,
+    signInUser,
+} = require('../controllers/userController');
 
 const router = express.Router();
 
+router.route('/').get(getUsers);
+router.route('/:id').get(getUser);
+router.route('/').post(saveUser);
+router.route('/:id').put(updateUser);
+router.route('/:id').delete(deleteUser);
 router.route('/current/info').get(validateToken, getCurrentUser);
+router.route('/email/:email').get(getUserByEmail);
 router.route('/sign-in').post(signInUser);
 
 module.exports = router;

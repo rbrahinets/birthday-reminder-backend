5d14ac22c55b5e22eef59689734678f7d7599940
rbrahinets <rostyslav.barhinets@gmail.com>
2024-05-04 15:20:13 +0300

    Add id of image for user

--- src/models/User.js
+++ src/models/User.js
@@ -19,6 +19,9 @@ const userSchema = mongoose.Schema(
             type: String,
             required: [true, 'Please add the password'],
         },
+        imageId: {
+            type: String,
+        },
     },
     {
         timestamps: true,

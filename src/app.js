3ce981df95a33dfcb773877eb810a66688d3e6ff
rbrahinets <rostyslav.barhinets@gmail.com>
2024-05-08 18:04:25 +0300

    Delete routes for images

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const friendRoutes = require('./routes/friendRoutes');
-const imageRoutes = require('./routes/imageRoutes');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json());
+
 app.use('/api/v1/friends', friendRoutes);
-app.use('/api/v1/images', imageRoutes);
 app.use('/api/v1/users', userRoutes);
 
 module.exports = app;

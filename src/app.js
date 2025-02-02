fce51e6c33dcd0364826b9c80f33c50792b4a100
Rostyslav Brahinets <rostyslav.brahinets@gmail.com>
2025-02-02 20:03:47 +0200

    Add origins for cors

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
-const express = require('express');
-const cors = require('cors');
-const birthdayRoutes = require('./routes/birthdayRoutes');
-const userRoutes = require('./routes/userRoutes');
+const express = require('express')
+const cors = require('cors')
+const birthdayRoutes = require('./routes/birthdayRoutes')
+const userRoutes = require('./routes/userRoutes')
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+const allowedOrigins = ['http://localhost:3000', 'https://birthday-reminder-app.pages.dev']
 
-app.use('/api/v1/birthdays', birthdayRoutes);
-app.use('/api/v1/users', userRoutes);
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+}))
 
-module.exports = app;
+app.use(express.json())
+
+app.use('/api/v1/birthdays', birthdayRoutes)
+app.use('/api/v1/users', userRoutes)
+
+module.exports = app
